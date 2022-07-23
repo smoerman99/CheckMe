0c71ac33c27d06091f8c84b81756da3158d0c1b0
Sander Moerman <smoerman99@gmail.com>
2022-07-24 00:03:52 +0200

    changed title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo 123',
       theme: ThemeData(
         //main section
         //default color the background
