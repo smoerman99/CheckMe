d067782926680dcf447c6d61670457630342aa7a
Sander Moerman <smoerman99@gmail.com>
2024-05-09 21:58:40 +0200

    added new function

---
+++ lib/Assets/GetBibleVerses.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:checkit/Entities/DailyVerse.dart';
+import 'package:http/http.dart' as http;
+
+Future<DailyVerse> fetchBibleVerse() async {
+  final _random = new Random();
+  var listOfBibleVerses = ['JHN.3.16', 'ECC.12.13', '1CO.10.31'];
+
+  Map<String, String> requestHeaders = {
+    'api-key': '31b591ef3138872c7b3722d569a17477'
+  };
+
+  final response = await http.get(
+      Uri.parse(
+          'https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-02/verses/' +
+              listOfBibleVerses[_random.nextInt(listOfBibleVerses.length)]),
+      headers: requestHeaders);
+
+  if (response.statusCode == 200) {
+    var json = jsonDecode(response.body) as Map<String, dynamic>;
+
+    return DailyVerse.fromJson(json['data']);
+  } else {
+    throw Exception('Failed to load DailyVerse');
+  }
+}
